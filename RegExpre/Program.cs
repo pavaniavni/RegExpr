e30b0d46799d024d2d472d89a4e8062fb83859f2
pavani <pavaniavni456@gmail.com>
2022-02-07 16:45:37 +0530

    added Regexpress

---
+++ RegExpre/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HandlingRegEx
+{
+    public class RegEx
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex("^[0-9]{4}$");
+            Match m = r.Match("9798skhdah");
+            if (m.Success)
+            {
+                Console.WriteLine("Mached the value " + m.Value);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Correct String Input");
+            }
+        }
+    }
+}
